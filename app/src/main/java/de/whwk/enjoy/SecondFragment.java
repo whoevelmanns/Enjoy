d0ec634c8003f9dd6b677eb8de44d147a63e4ac6
Werner Hövelmanns <info@whwk.de>
2021-08-15 12:46:16 +0200

    Initial commit

---
+++ app/src/main/java/de/whwk/enjoy/SecondFragment.java
@@ -0,0 +1,46 @@
+package de.whwk.enjoy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import de.whwk.enjoy.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_LoginFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/de/whwk/enjoy/Shared.java
@@ -0,0 +1,6 @@
+package de.whwk.enjoy;
+
+public class Shared {
+    public static boolean LoginOk(String user,String password) {
+    }
+}
