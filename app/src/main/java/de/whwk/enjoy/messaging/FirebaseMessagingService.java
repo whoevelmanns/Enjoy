3fad52c2898db17c4499f9fa501a4a024f0fd547
Werner Hövelmanns <info@whwk.de>
2021-10-12 15:10:19 +0200

    Integration Message Fehlermeldung bei Login

---
+++ app/src/main/java/de/whwk/enjoy/messaging/FirebaseMessagingService.java
@@ -0,0 +1,43 @@
+package de.whwk.enjoy.messaging;
+
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.google.firebase.messaging.RemoteMessage;
+
+@SuppressWarnings("CommentedOutCode")
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+  private final String TAG = this.getClass().getName();
+
+  @Override
+  public void onNewToken(@NonNull String s) {
+    super.onNewToken(s);
+  }
+
+  @Override
+  public void onMessageReceived(RemoteMessage remoteMessage) {
+    // ...
+
+    // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+    Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+    // Check if message contains a data payload.
+    if (remoteMessage.getData().size() > 0) {
+      Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+//      if (/* Check if data needs to be processed by long running job */ true) {
+//        // For long-running tasks (10 seconds or more) use WorkManager.
+//        scheduleJob();
+//      } else {
+//        // Handle message within 10 seconds
+//        handleNow();
+//      }
+    }
+
+    // Check if message contains a notification payload.
+    if (remoteMessage.getNotification() != null) {
+      Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+    }
+
+    // Also if you intend on generating your own notifications as a result of a received FCM
+    // message, here is where that should be initiated. See sendNotification method below.
+  }
+}
