f31c5a414adef0042ef91dc484f6dff7faccf8b3
Werner Hövelmanns <info@whwk.de>
2021-10-12 19:48:13 +0200

    Bugfixing

--- app/src/main/java/de/whwk/enjoy/vote/VoteAdapter.java
+++ app/src/main/java/de/whwk/enjoy/vote/VoteAdapter.java
@@ -1,15 +1,19 @@
 package de.whwk.enjoy.vote;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import de.whwk.enjoy.R;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class VoteAdapter extends BaseAdapter {
   @SuppressWarnings("unused")
@@ -17,6 +21,8 @@ public class VoteAdapter extends BaseAdapter {
   private final VotingFragment votingFragment;
   Context context;
   HashMap<Integer, VoteModel> map;
+  Map<Integer, View> myViews = new HashMap<>();
+
 
   public VoteAdapter(VotingFragment votingFragment, Context context, HashMap<Integer, VoteModel> map) {
     this.votingFragment = votingFragment;
@@ -35,50 +41,79 @@ public class VoteAdapter extends BaseAdapter {
   }
 
   @Override
-  public long getItemId(int i) {
-    return i;
+  public long getItemId(int position) {
+    return Objects.requireNonNull(map.get(position)).getEventId();
   }
 
+  @SuppressLint("NonConstantResourceId")
   @Override
   public View getView(final int position, View convertView, ViewGroup parent) {
-    if (convertView == null) {
+    ViewHolder v;
+    //TODO Hier muss ich nochmal ran
+    if (true || !myViews.containsKey(position)) {
       convertView = LayoutInflater.from(context).inflate(R.layout.event_list_item, parent, false);
+      v = new ViewHolder();
+      v.title = convertView.findViewById(R.id.event);
+      v.date = convertView.findViewById(R.id.date);
+      v.location = convertView.findViewById(R.id.location);
+      v.statusGrp = convertView.findViewById(R.id.status);
+      v.btnNo = convertView.findViewById(R.id.rbtn_0);
+      v.btnYes = convertView.findViewById(R.id.rbtn_1);
+      v.statusGrp.setOnCheckedChangeListener((radioGroup, id) -> {
+        int newStatus = -1;
+        switch (id) {
+          case R.id.rbtn_0:
+            newStatus = 0;
+            break;
+          case R.id.rbtn_1:
+            newStatus = 1;
+            break;
+        }
+        if (newStatus >= 0) {
+          VoteModel voteModel = map.get(position);
+          assert voteModel != null;
+          voteModel.setStatus(newStatus);
+          votingFragment.vote(voteModel.getEventId(), newStatus);
+        }
+      });
+      convertView.setTag(v);
+      myViews.put(position, convertView);
+    } else {
+      convertView = myViews.get(position);
+      assert convertView != null;
+      v = (ViewHolder) convertView.getTag();
     }
     VoteModel voteModel = map.get(position);
     assert voteModel != null;
-    ((TextView) convertView.findViewById(R.id.event)).setText(voteModel.getTitel());
-    ((TextView) convertView.findViewById(R.id.date)).setText(voteModel.getDate());
-    TextView tv = convertView.findViewById(R.id.location);
+    v.title.setText(voteModel.getTitel());
+    v.date.setText(voteModel.getDate());
     String t = voteModel.getLocation();
     if (t == null) {
-      tv.setVisibility(View.GONE);
+      v.location.setVisibility(View.GONE);
     } else {
-      tv.setText(t);
+      v.location.setText(t);
+      v.location.setVisibility(View.VISIBLE);
     }
+    v.statusGrp.clearCheck();
     Integer status = voteModel.getStatus();
-    Integer event_id = voteModel.getEventId();
-    RadioButton btn;
-    btn = convertView.findViewById(R.id.rbtn_0);
-    if (status != null && status.equals(0)) {
-      btn.setChecked(true);
-    }
-    btn.setOnCheckedChangeListener((compoundButton, b) -> {
-      if (b) {
-        voteModel.setStatus(0);
-        votingFragment.vote(event_id, 0);
+    if (status != null) {
+      switch (status) {
+        case 0:
+          v.btnNo.setChecked(true);
+          break;
+        case 1:
+          v.btnYes.setChecked(true);
+          break;
       }
-    });
-    btn = convertView.findViewById(R.id.rbtn_1);
-    if (status != null && status.equals(1)) {
-      btn.setChecked(true);
     }
-    btn.setOnCheckedChangeListener((compoundButton, b) -> {
-      if (b) {
-        voteModel.setStatus(1);
-        votingFragment.vote(event_id, 1);
-      }
-    });
     return convertView;
+  }
+
+  static class ViewHolder {
+    RadioButton btnNo;
+    RadioButton btnYes;
+    TextView title, date, location;
+    RadioGroup statusGrp;
   }
 }
 
