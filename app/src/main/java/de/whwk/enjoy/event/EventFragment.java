d2f1798fcb6141bd25c38860096e9cc9dfb225f3
Werner Hövelmanns <info@whwk.de>
2021-10-14 12:06:07 +0200

    Details der Mitsänger

--- app/src/main/java/de/whwk/enjoy/event/EventFragment.java
+++ app/src/main/java/de/whwk/enjoy/event/EventFragment.java
@@ -1,12 +1,16 @@
 package de.whwk.enjoy.event;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -27,10 +31,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class EventFragment extends Fragment {
   private final String TAG = this.getClass().getName();
@@ -74,6 +77,8 @@ public class EventFragment extends Fragment {
         EventAdapter adapter = new EventAdapter(requireView().getContext(), map);
         ListView listView = requireView().findViewById(R.id.listview_events);
         listView.setAdapter(adapter);
+        listView.setLongClickable(true);
+        listView.setOnItemLongClickListener((adapterView, view, position, id) -> EventLongClicked(Objects.requireNonNull(map.get(position))));
         PieChart chart = requireView().findViewById(R.id.chart);
         List<PieEntry> entries = new ArrayList<>();
         for (Integer  index : map.keySet()) {
@@ -119,6 +124,26 @@ public class EventFragment extends Fragment {
       }
     };
     mRequestQueue.add(stringRequest);
+  }
+
+  @SuppressLint("ClickableViewAccessibility")
+  private boolean EventLongClicked(EventModel event) {
+    @SuppressLint("InflateParams")
+    View view = ((LayoutInflater) requireActivity().getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.popup,null);
+    ((TextView) view.findViewById(R.id.members)).setText(event.getMembers());
+    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+    boolean focusable = true; // lets taps outside the popup also dismiss it
+    final PopupWindow popupWindow = new PopupWindow(view, width, height, focusable);
+    // show the popup window
+    // which view you pass in doesn't matter, it is only used for the window tolken
+    popupWindow.showAtLocation(requireView(), Gravity.CENTER, 0, 0);
+    // dismiss the popup window when touched
+    view.setOnTouchListener((v, event1) -> {
+      popupWindow.dismiss();
+      return true;
+    });
+    return true;
   }
 
   @Override
--- app/src/main/java/de/whwk/enjoy/event/EventModel.java
+++ app/src/main/java/de/whwk/enjoy/event/EventModel.java
@@ -1,20 +1,21 @@
 package de.whwk.enjoy.event;
 
 import lombok.Getter;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 @Getter
 public class EventModel {
+  private final String voiceName;
+  private final JSONObject voice;
   private int yes;
   private int no;
   private int total;
-  private final String voiceName;
-  private final JSONObject voice;
 
   public EventModel(String voiceName, JSONObject voice) {
     this.voice = voice;
-    this.voiceName=voiceName;
+    this.voiceName = voiceName;
     try {
       yes = voice.getInt("yes");
       no = voice.getInt("no");
@@ -25,6 +26,26 @@ public class EventModel {
   }
 
   public int getUndefined() {
-    return total-yes-no;
+    return total - yes - no;
+  }
+
+  public String getMembers() {
+    StringBuilder answer = new StringBuilder(voiceName + "\n\n");
+    try {
+      JSONObject users = voice.getJSONObject("user");
+      JSONArray names = users.names();
+      if (names != null) {
+        for (int i = 0; i < names.length(); i++) {
+          String name = names.getString(i);
+          JSONObject user = users.getJSONObject(name);
+          if ("1".equals(user.getString("status"))) {
+            answer.append(name).append("\n");
+          }
+        }
+      }
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return answer.toString();
   }
 }
